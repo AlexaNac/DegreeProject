de12f08e93f9e309e8aa679a3bc5b2ef0ba3b488
alexandra_nac@yahoo.com <alexandra_nac@yahoo.com>
2017-06-14 04:59:16 +0300

    Initial commit Entity, Identity, Linkedin, Employee forms.

---
+++ WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Data.Entity;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<employee> employees;
+            using (var _context = new ProjectDBContext())
+            {
+                employees = _context.employees.ToList();
+            }
+            return View(employees);
+        }
+
+        public ActionResult New()
+        {
+            employee employeeModel;
+            using (var _context = new ProjectDBContext())
+            {
+                var departmentsList = _context.departments.ToList();
+                var jobsList = _context.jobs.ToList();
+                var managersList = _context.employees.ToList();
+                var usersList = _context.AspNetUsers.ToList();
+                employeeModel = new employee
+                {
+                    jobs = jobsList,
+                    departments = departmentsList,
+                    employees1 = managersList,
+                    users = usersList
+                };
+            }
+            return View(employeeModel);
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            employee emp;          
+            using (var _context = new ProjectDBContext())
+            {
+                emp = _context.employees.FirstOrDefault(e => e.employee_id == id);
+            }       
+            if(emp == null)
+            {
+                RedirectToAction("Index", "Employee");// HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            employee emp;
+            using (var _context = new ProjectDBContext())
+            {
+                emp = _context.employees.Include(e => e.job).Include(e => e.AspNetUser).Include(e => e.department).Include(e => e.employee1).FirstOrDefault(e => e.employee_id == id);
+            }
+            if (emp == null)
+            {
+                RedirectToAction("Index", "Employee");// HttpNotFound();
+            }
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Save(employee emp)
+        {
+            if (!ModelState.IsValid)
+            {
+                RedirectToAction("Edit","Employee",emp.employee_id);
+            }
+            employee employeeDB;
+            using (var _context = new ProjectDBContext()) {
+                employeeDB = _context.employees.Single(e => e.employee_id == emp.employee_id);
+                TryUpdateModel(employeeDB);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Employee");
+            }           
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(employee emp)
+        {
+            if (!ModelState.IsValid)
+            {
+                RedirectToAction("New","Employee", emp);
+            }
+            employee newemp;
+            using (var _context = new ProjectDBContext())
+            {
+                    newemp = new employee
+                    {
+                        employee_id = Guid.NewGuid(),
+                        first_name = emp.first_name,
+                        last_name = emp.last_name,
+                        birth_date = emp.birth_date,
+                        hire_date = emp.hire_date,
+                        job_id = emp.job_id,
+                        department_id = emp.department_id,
+                        phone_number = emp.phone_number,
+                        email = emp.email,
+                        salary = emp.salary,
+                        user_id = emp.user_id,
+                        manager_id = emp.manager_id
+                    };
+                _context.employees.Add(newemp);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Edit","Employee", newemp.employee_id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            employee employeeDB;
+            using (var _context = new ProjectDBContext())
+            {
+                employeeDB = _context.employees.Single(e => e.employee_id == id);
+                _context.employees.Remove(employeeDB);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Employee");
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/LinkedinController.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using WebApplication1.Models;
+using Microsoft.AspNet.Identity;
+
+namespace WebApplication1.Controllers
+{
+    public class LinkedinController : Controller
+    {
+        // GET: Linkedin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Authentication(string code, string state)
+        {
+            //This method gets the profile of the user and his informations     
+            try
+            {
+                string userid = User.Identity.GetUserId();
+
+                var client = new RestClient("https://www.linkedin.com/oauth/v2/accessToken");
+                var request = new RestRequest(Method.POST);
+                request.AddParameter("grant_type", "authorization_code");
+                request.AddParameter("code", code);
+                request.AddParameter("redirect_uri", "http://localhost:54480/Linkedin/Authentication");
+                request.AddParameter("client_id", "867v0zbln7wqzu");
+                request.AddParameter("client_secret", "HOa0hQOb7396VsH4");
+
+                IRestResponse response = client.Execute(request);
+                JObject content = JObject.Parse(response.Content);
+                String f = content["access_token"].ToString();
+
+                //must parse the content json
+                //Get Profile Details  
+                client = new RestClient("https://api.linkedin.com/v1/people/~?oauth2_access_token=" + content["access_token"].ToString() + " & format=json");
+                request = new RestRequest(Method.GET);
+                response = client.Execute(request);
+                StringBuilder output = new StringBuilder();
+                var xmlString = response.Content;
+
+                String profile;
+                using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
+                {
+                    reader.ReadToFollowing("url");
+                    profile = reader.ReadElementContentAsString();
+                }
+                if (userid != null)
+                {
+                    return await UpdateUser(profile, userid);
+                }
+                else
+                {
+                    return await LoginUser(profile);
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UpdateUser(String profile, String userid)
+        {
+            using (var _context = new ProjectDBContext())
+            {
+                AspNetUser user = _context.AspNetUsers.FirstOrDefault(e => e.Id == userid);
+                user.LinkedinUrl = profile;
+                if (TryUpdateModel(user) == true)
+                    await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Manage");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> LoginUser(String profile)
+        {
+            using (var _context = new ProjectDBContext())
+           {
+                AspNetUser user = _context.AspNetUsers.FirstOrDefault(e => e.LinkedinUrl == profile);
+                if (user != null) { //login
+                    LoginViewModel model = new LoginViewModel
+                    {
+                        Email = user.Email,
+                        Password = user.PasswordHash,
+                        RememberMe = false
+                    };
+                    AccountController ac = new AccountController();
+                    await ac.Login(model, null);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    return RedirectToAction("Login", "Account");
+            }
+        }
+    }
+}
